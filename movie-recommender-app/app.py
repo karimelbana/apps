b5da3a82f3852bc4424e8697a3bb40da20487454
Karim Elbana <karimelbana@Karims-MacBook-Pro.local>
2021-10-28 15:08:57 +0200

    organize again 2

--- movie-recommender-app/app.py
+++ movie-recommender-app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("main.html", title = "Rendom Recommendor")
+    return render_template("main.html", title = "Kickass Movie Recommender")
 
 
 @app.route('/recommendations')
--- movie-recommender-app/cs_model.py
+++ movie-recommender-app/cs_model.py
@@ -6,8 +6,8 @@ import numpy as np
 
 #get data
 
-movies = pd.read_csv('../Data_small/movies.csv')
-ratings_data = pd.read_csv('../Data_small/ratings.csv')
+movies = pd.read_csv('./Data_small/movies.csv')
+ratings_data = pd.read_csv('./Data_small/ratings.csv')
 movies_rating = pd.merge(movies, ratings_data, on='movieId')
 
 df = pd.pivot_table(movies_rating, values='rating', index='userId',
